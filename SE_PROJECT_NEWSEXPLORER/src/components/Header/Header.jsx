fb676d38447efe648f3fe60f186a98d83d0cb995
David Bonilla <thegrindnet@gmail.com>
2026-05-18 00:42:11 -0600

    Add Roboto font in vendor, add Navigation files

---
+++ SE_PROJECT_NEWSEXPLORER/src/components/Header/Header.jsx
@@ -0,0 +1,16 @@
+function Header() {
+  return (
+    <header className="header">
+      <img
+        className="header_logo"
+        src="../../assets/logo.svg"
+        alt="NewsExplorer Logo"
+      />
+      <a href="#">Home</a>
+    </header>
+  );
+}
+
+export default Header;
+
+// Left off at 8:47 on video
